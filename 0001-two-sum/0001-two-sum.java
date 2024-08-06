6b13f4ceff56820bba9cebcb1329434ed628f3de
piratE-oF-interneT <145834264+piratE-oF-interneT@users.noreply.github.com>
2024-08-06 20:47:00 +0300

    Time: 4 ms (63.51%), Space: 44.7 MB (57.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,21 +1,45 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        int[] ans = new int[2];
 
+        int[] dup = new int[nums.length];
+        int[] ans = new int[2];
         for(int i=0;i<nums.length;i++){
-            int flag=0;
-            for(int j=i+1;j<nums.length;j++){
-                if(target-nums[i]==nums[j]){
-                    ans[0]=i;
-                    ans[1]=j;
-                    flag=1;
-                }
+            dup[i]=nums[i];
+        }
+
+        Arrays.sort(nums);
+
+        int i=0;
+        int j=nums.length-1;
+        int val1 = 0;
+        int val2 = 0;
+        while(i<j){
+            if(nums[i]+nums[j]>target){
+                j--;
             }
-            if(flag==1){
+            else if(nums[i]+nums[j]<target){
+                i++;
+            }
+            else{
+                val1 = nums[i];
+                val2 = nums[j];
                 break;
             }
         }
+        j=0;
+        int k=0;
+        while(j<2){
+            if(dup[k]==val1){
+                ans[j++]=k++;
+            }
+            else if(dup[k]==val2){
+                ans[j++]=k++;
+            }
+            else{
+                k++;
+            }
+        }
         return ans;
+        
     }
 }
